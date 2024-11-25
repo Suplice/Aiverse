e30d941bbe85cbeb2a551d8aee92c6393cd02263
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-25 23:14:31 +0100

    Initialize client project

---
+++ Client/src/App.tsx
@@ -0,0 +1,11 @@
+import FirstComponent from "./Components/FirstComponent";
+
+function App() {
+  return (
+    <>
+      <FirstComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ Client/src/Components/FirstComponent.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+
+const FirstComponent: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          import.meta.env.VITE_API_URL + "/api/GetWeatherForecast"
+        );
+
+        console.log(response);
+
+        const data = await response.json();
+
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {count}
+      <button
+        className="px-10 py-5 border-2 border-black"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click me!
+      </button>
+    </div>
+  );
+};
+
+export default FirstComponent;
