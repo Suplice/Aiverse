30bb8ad610c3555a3fb52f283da9a9603139b68a
Mateusz Duma <mateuszsuplice@gmail.com>
2024-12-10 17:57:19 +0100

    feat: integrate AuthProvider into AppRouter for authentication context

--- Client/src/Components/AppRouter/AppRouter.tsx
+++ Client/src/Components/AppRouter/AppRouter.tsx
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from "../../Pages/SignUp/SignUp";
 import SignIn from "../../Pages/SignIn/SignIn";
 import FirstComponent from "../FirstComponent";
+import { AuthProvider } from "../../Utils/Context/AuthContext";
 
 const AppRouter = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="auth">
-          <Route path="SignUp" element={<SignUp />}></Route>
-          <Route path="SignIn" element={<SignIn />}></Route>
-        </Route>
-        <Route path="*" element={<FirstComponent />}></Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="auth">
+            <Route path="SignUp" element={<SignUp />}></Route>
+            <Route path="SignIn" element={<SignIn />}></Route>
+          </Route>
+          <Route path="*" element={<FirstComponent />}></Route>
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 };
