03929a54cf24491dfdc41742eff5cfe7cbf43a1a
Mateusz Duma <mateuszsuplice@gmail.com>
2024-12-11 20:04:13 +0100

    feat: add AppPreview component for displaying application preview

---
+++ Client/src/Components/AppPreview/AppPreview.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppPreview = () => {
+  return (
+    <div className="flex flex-col w-full h-full ">
+      <div></div>
+    </div>
+  );
+};
+
+export default AppPreview;
