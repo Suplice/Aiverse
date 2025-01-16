a6b360e33f4a29693772d84642e9e3be53d02a22
Mateusz Duma <mateuszsuplice@gmail.com>
2025-01-16 22:58:09 +0100

    feat: update LandingServiceCard to display index and simplify SearchPageServices component

--- Client/src/Pages/SearchServices/SearchServices.tsx
+++ Client/src/Pages/SearchServices/SearchServices.tsx
@@ -4,26 +4,11 @@ import SearchPageServices from "../../Components/SearchPageComponents/SearchPage
 import SearchPagePopularServices from "../../Components/SearchPageComponents/SearchPagePopularServices";
 
 const SearchServices = () => {
-  // const [searchParams, setSearchParams] = useSearchParams();
-
-  // Extract parameters from the URL
-  // const category = searchParams.get("category") || "all";
-  // const searchText = searchParams.get("searchText") || "";
-  // const priceRange = searchParams.get("priceRange") || "any";
-
-  // const updateSearch = () => {
-  //   setSearchParams({
-  //     category: "electronics",
-  //     searchText: "laptop",
-  //     priceRange: "100-500",
-  //   });
-  // };
-
   return (
     <div className="flex flex-col p-6 w-full bg-[#121212]">
       <LandingNavbar />
       <div className="flex flex-row gap-4 lg:p-10 md:p-6 sm:p-2  ">
-        <SearchPageServices searchText={"test"} />
+        <SearchPageServices />
         <SearchPagePopularServices />
       </div>
     </div>
