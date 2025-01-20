65f340e176652ec150a810e13979f2cc0687c9b4
Mateusz Duma <mateuszsuplice@gmail.com>
2025-01-20 21:42:34 +0100

    feat: refactor SearchServices and related components to utilize Block and BlockTextField for improved layout and consistency

--- Client/src/Components/SearchPageComponents/FilterButtons.tsx
+++ Client/src/Components/SearchPageComponents/FilterButtons.tsx
@@ -5,6 +5,9 @@ import { IoMdArrowDropdown } from "react-icons/io";
 import { Range } from "react-range";
 import { useLocation, useNavigate } from "react-router";
 import { SearchParams } from "../../Utils/Models/SearchParams";
+import TextField from "../UI/TextField";
+import Button from "../UI/Button";
+import Block from "../UI/Block";
 
 const Categories = ["Video", "Music", "Art", "LLM"];
 
@@ -60,24 +63,31 @@ const FilterButtons: React.FC<FilterButtonsProps> = ({
   return (
     <>
       <div className="relative">
-        <div
-          className="px-4 py-2 bg-blue-500 text-center flex flex-row hover:cursor-pointer hover:bg-blue-500/80 items-center gap-2 justify-center rounded-md transition-all duration-200"
+        <Block
+          className="px-4 py-2 bg-blue-500 text-center hover:cursor-pointer hover:bg-blue-500/80 rounded-md transition-all duration-200"
+          direction="row"
+          align="center"
+          justify="center"
+          gap={2}
           onClick={() => setIsCategoryMenuVisible(!isCategoryMenuVisible)}
         >
-          <p className="select-none text-white">Categories</p>
+          <TextField value="Categories" className="select-none" color="white" />
           <IoMdArrowDropdown
             className={`${
               isCategoryMenuVisible ? " rotate-180 " : "rotate-0 "
             } transition-all duration-200 text-white`}
             size={24}
           />
-        </div>
+        </Block>
+
         {isCategoryMenuVisible && (
           <div className="absolute mt-2 w-full rounded border shadow-lg z-30 border-[#3B3B3D]">
             {Categories.map((value, index) => (
-              <div
+              <Block
+                className="px-4 py-2 hover:bg-[#232325] bg-[#121212] cursor-pointer"
                 key={index}
-                className="flex items-center px-4 py-2 hover:bg-[#232325] bg-[#121212] cursor-pointer"
+                align="center"
+                direction="row"
                 onClick={() => {
                   const checkbox = document.getElementById(
                     `category-checkbox-${index}`
@@ -101,29 +111,44 @@ const FilterButtons: React.FC<FilterButtonsProps> = ({
                   value={value}
                   checked={searchParams.categories.includes(value)}
                 ></input>
-                <p className="select-none text-white">{value}</p>
-              </div>
+
+                <TextField className="select-none" color="white">
+                  {value}
+                </TextField>
+              </Block>
             ))}
           </div>
         )}
       </div>
       <div className="relative">
-        <div
-          className="px-4 py-2 bg-blue-500 text-center flex flex-row hover:cursor-pointer hover:bg-blue-500/80 items-center gap-2 justify-center rounded-md transition-all duration-200"
+        <Block
+          className="px-4 py-2 bg-blue-500 text-center hover:cursor-pointer hover:bg-blue-500/80 rounded-md transition-all duration-200"
+          direction="row"
+          align="center"
+          justify="center"
+          gap={2}
           onClick={() => setIsPriceMenuVisible(!isPriceMenuVisible)}
         >
-          <p className="select-none text-white">Price range</p>
+          <TextField
+            value="Price range"
+            className="select-none"
+            color="white"
+          />
           <IoMdArrowDropdown
             className={`${
               isPriceMenuVisible ? " rotate-180 " : " rotate-0 "
             } transition-all duration-200 text-white`}
             size={24}
           />
-        </div>
+        </Block>
         {isPriceMenuVisible && (
           <div className="absolute mt-2 w-fit bg-[#121212] border border-[#3B3B3D] rounded-md shadow-lg z-30 p-4">
-            <div className="flex flex-col items-center">
-              <p className="mb-2 select-none text-white">Select Price Range</p>
+            <Block align="center" direction="column">
+              <TextField
+                value="Select Price Range"
+                className="select-none"
+                color="white"
+              />
               <Range
                 step={1}
                 min={0}
@@ -174,7 +199,7 @@ const FilterButtons: React.FC<FilterButtonsProps> = ({
                   </div>
                 )}
               />
-              <div className="flex mt-4 gap-2 flex-wrap">
+              <Block className="flex-wrap mt-4" gap={2}>
                 <input
                   type="number"
                   value={searchParams.priceRange[0]}
@@ -197,32 +222,42 @@ const FilterButtons: React.FC<FilterButtonsProps> = ({
                   }
                   className="border p-1 rounded select-none bg-zinc-600 text-white border-[#3B3B3D] outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                 />
-              </div>
-            </div>
+              </Block>
+            </Block>
           </div>
         )}
       </div>
-      <div className="relative flex flex-row gap-4 items-center ">
-        <Tooltip label="Remove filters" position="bottom" withArrow>
-          <div>
-            <FcClearFilters size={24} className="hover:cursor-pointer" />
-          </div>
-        </Tooltip>
-        <button
-          id="apply-filters"
-          onClick={applyFilters}
-          className="bg-green-700 hover:bg-green-600 text-white px-4 py-2 rounded-md transition-colors duration-200 "
-          disabled={isFiltering}
-        >
-          {isFiltering ? (
-            <div className=" w-full h-full px-2 py-2 flex items-center justify-center gap-2">
-              <div className=" w-4 h-4 border-4 border-black border-dotted rounded-full animate-spin"></div>
+      <Block className="relative " align="center" gap={4} direction="row">
+        <div className="relative flex flex-row gap-4 items-center ">
+          <Tooltip label="Remove filters" position="bottom" withArrow>
+            <div>
+              <FcClearFilters size={24} className="hover:cursor-pointer" />
             </div>
-          ) : (
-            "Apply Filters"
-          )}
-        </button>
-      </div>
+          </Tooltip>
+
+          <Button
+            id="apply-filters"
+            onClick={applyFilters}
+            className="bg-green-700 hover:bg-green-600 px-4 py-2 rounded-md"
+            TextColor="white"
+          >
+            {isFiltering ? (
+              <Block
+                className="w-full h-full px-2 py-2"
+                align="center"
+                justify="center"
+                gap={2}
+              >
+                <div className=" w-full h-full px-2 py-2 flex items-center justify-center gap-2">
+                  <div className=" w-4 h-4 border-4 border-black border-dotted rounded-full animate-spin"></div>
+                </div>
+              </Block>
+            ) : (
+              "Apply Filters"
+            )}
+          </Button>
+        </div>
+      </Block>
     </>
   );
 };
