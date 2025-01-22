172eb2a9df2dd6dedf6e924f6dae97d8aa56b96c
KamilBorowiecki <kamil.borowiecki@op.pl>
2025-01-22 23:15:11 +0100

    fix: dark user panel

--- Client/src/Components/UserPanelComponents/UserSettings.tsx
+++ Client/src/Components/UserPanelComponents/UserSettings.tsx
@@ -20,146 +20,14 @@ const UserSettings = () => {
   const [passwordError, setPasswordError] = useState<string | null>(null);
   const [passwordSuccess, setPasswordSuccess] = useState<string | null>(null);
 
-
-
-  const updateUserName = async (newName: string) => {
-    if (!user) {
-      console.error("User data is not available");
-      return;
-    }
-
-    if (newName.trim() === "") {
-      alert("Name cannot be empty!");
-      return;
-    }
-
-    try {
-      const updatedUserData = {
-        ...user,
-        Name: newName, // Zmieniamy tylko pole Name
-      };
-
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/user/${user.Id}`,
-        {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(updatedUserData),
-          credentials: "include",
-        }
-      );
-
-      if (response.ok) {
-        console.log("User name updated successfully");
-        setUserName(newName); 
-      } else {
-        console.error("Failed to update user name");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   const handleChangeName = async () => {
-    try {
-      await updateUserName(userName);
-      setIsEditingName(false);
-      console.log("New name:", userName);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const updateUserEmail = async (newEmail: string) => {
-    if (!user) {
-      console.error("User data is not available");
-      return;
-    }
-
-    if (newEmail.trim() === "") {
-      alert("Name cannot be empty!");
-      return;
-    }
-
-    try {
-      const updatedUserData = {
-        ...user,
-        Email: newEmail, 
-      };
-
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/user/${user.Id}`,
-        {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(updatedUserData),
-          credentials: "include",
-        }
-      );
-
-      if (response.ok) {
-        console.log("User name updated successfully");
-        setUserEmail(newEmail); 
-      } else {
-        console.error("Failed to update user name");
-      }
-    } catch (error) {
-      console.error(error);
-    }
+    setIsEditingName(false);
+    console.log("New name:", userName);
   };
 
   const handleChangeEmail = async () => {
-    try {
-      await updateUserEmail(userEmail);
-      setIsEditingEmail(false);
-      console.log("New email:", userEmail);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const updateUserPassword = async (newUserPassword: string) => {
-    if (!user) {
-      console.error("User data is not available");
-      return;
-    }
-
-    if (newUserPassword.trim() === "") {
-      alert("Name cannot be empty!");
-      return;
-    }
-
-    try {
-      const updatedUserData = {
-        ...user,
-        Password: newUserPassword, 
-      };
-
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/user/${user.Id}`,
-        {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(updatedUserData),
-          credentials: "include",
-        }
-      );
-
-      if (response.ok) {
-        console.log("User name updated successfully");
-        setNewPassword(newPassword); 
-      } else {
-        console.error("Failed to update user name");
-      }
-    } catch (error) {
-      console.error(error);
-    }
+    setIsEditingEmail(false);
+    console.log("New email:", userEmail);
   };
 
   const handleChangePassword = async () => {
@@ -167,53 +35,24 @@ const UserSettings = () => {
       setPasswordError("New password and confirmation do not match.");
       return;
     }
-
-    if (newPassword.length < 6) {
-      setPasswordError("Password must be at least 6 characters long.");
-      return;
-    }
-
     setPasswordError(null);
-    console.log("Old password:", oldPassword);
-    console.log("New password:", newPassword);
-
     setPasswordSuccess("Password changed successfully!");
-    setOldPassword("");
-    setNewPassword("");
-    setConfirmNewPassword("");
-
-    try {
-      await updateUserPassword(newPassword);
-      setIsChangingPassword(false);
-      console.log("New password:", newPassword);
-    } catch (error) {
-      console.error(error);
-    }
+    setIsChangingPassword(false);
+    console.log("New password:", newPassword);
   };
 
-
-  
-
   return (
-    <Block className="p-4 bg-white rounded-lg" direction="column" gap={6}>
-      <Block
-        className="bg-gray-100 p-6 rounded-lg shadow-md border-4 md:flex-row"
-        justify="start"
-        align="center"
-        direction="column"
-        gap={4}
-      >
-        <BlockTextField className="text-lg" value="Name:" />
+    <Block className="p-4 bg-[#121212] rounded-lg text-white" direction="column" gap={6}>
+      <Block className="bg-[#2E2E2E] p-6  rounded-lg shadow-md border-4 md:flex-row" justify="start" align="center" direction="column" gap={4}>
+        <BlockTextField className="text-lg text-white" value="Name:" />
         {!isEditingName ? (
-          <Block className="w-full md:flex-row" align="center" justify="between" gap={4} direction="column" >
-            <TextField value={user?.Name} className="text-lg flex-grow" />
-            {user?.Provider === "EMAIL" && (
-              <Button
-                value="Change"
-                onClick={() => setIsEditingName(true)}
-                className="px-6 py-3 rounded-lg shadow-md focus:outline-none transition duration-300 border-4 bg-white text-black hover:bg-black hover:text-white"
-              />
-            )}
+          <Block className="w-full md:flex-row" align="center" justify="between" gap={4} direction="column">
+            <TextField value={user?.Name} className="text-lg flex-grow text-white bg-transparent" />
+            <Button
+              value="Change"
+              onClick={() => setIsEditingName(true)}
+              className="px-6 py-3 rounded-lg shadow-md focus:outline-none transition duration-300 border-4 bg-[#1E1E1E] text-white hover:bg-white hover:text-black"
+            />
           </Block>
         ) : (
           <Block direction="row" gap={2}>
@@ -221,35 +60,27 @@ const UserSettings = () => {
               type="text"
               value={userName}
               onChange={(e) => setUserName(e.target.value)}
-              className="px-4 py-2 border rounded w-full"
+              className="px-4 py-2 border rounded w-full bg-[#121212] text-white"
             />
             <Button
               value="Save"
               onClick={handleChangeName}
-              className="px-6 py-3 rounded-lg shadow-md focus:outline-none transition duration-300 border-4 bg-white text-green-500 hover:bg-green-500 hover:text-white"
+              className="px-6 py-3 rounded-lg shadow-md focus:outline-none transition duration-300 border-4 bg-[#1E1E1E] text-green-500 hover:bg-green-500 hover:text-white"
             />
           </Block>
         )}
       </Block>
 
-      <Block
-        className="bg-gray-100 p-6 rounded-lg shadow-md border-4 md:flex-row"
-        justify="start"
-        align="center"
-        direction="column"
-        gap={4}
-      >
-        <BlockTextField className="text-lg" value="Email:" />
+      <Block className="bg-[#2E2E2E] p-6 rounded-lg shadow-md border-4 md:flex-row" justify="start" align="center" direction="column" gap={4}>
+        <BlockTextField className="text-lg text-white" value="Email:" />
         {!isEditingEmail ? (
-          <Block className="w-full md:flex-row" align="center" justify="between" gap={4} direction="column" >
-            <TextField value={user?.Email} className="text-lg flex-grow" />
-            {user?.Provider === "EMAIL" && (
-              <Button
-                value="Change"
-                onClick={() => setIsEditingEmail(true)}
-                className="px-6 py-3 rounded-lg shadow-md focus:outline-none transition duration-300 border-4 bg-white text-black hover:bg-black hover:text-white"
-              />
-            )}
+          <Block className="w-full md:flex-row" align="center" justify="between" gap={4} direction="column">
+            <TextField value={user?.Email} className="text-lg flex-grow text-white bg-transparent" />
+            <Button
+              value="Change"
+              onClick={() => setIsEditingEmail(true)}
+              className="px-6 py-3 rounded-lg shadow-md focus:outline-none transition duration-300 border-4 bg-[#1E1E1E] text-white hover:bg-white hover:text-black"
+            />
           </Block>
         ) : (
           <Block direction="row" gap={2}>
@@ -257,66 +88,56 @@ const UserSettings = () => {
               type="text"
               value={userEmail}
               onChange={(e) => setUserEmail(e.target.value)}
-              className="px-4 py-2 border rounded w-full"
+              className="px-4 py-2 border rounded w-full bg-[#121212] text-white"
             />
             <Button
               value="Save"
               onClick={handleChangeEmail}
-              className="px-6 py-3 rounded-lg shadow-md focus:outline-none transition duration-300 border-4 bg-white text-green-500 hover:bg-green-500 hover:text-white"
+              className="px-6 py-3 rounded-lg shadow-md focus:outline-none transition duration-300 border-4 bg-[#1E1E1E] text-green-500 hover:bg-green-500 hover:text-white"
             />
           </Block>
         )}
       </Block>
 
-      {user?.Provider === "EMAIL" && (
-        <Block
-          className="bg-gray-100 p-6 rounded-lg shadow-md border-4"
-          direction="column"
-          gap={4}
-        >
-          <Button
-            value="Change Password"
-            onClick={() => setIsChangingPassword(!isChangingPassword)}
-            className="px-6 py-3 rounded-lg shadow-md focus:outline-none transition duration-300 border-4 bg-white text-black hover:bg-black hover:text-white"
-          />
-          {isChangingPassword && (
-            <Block direction="column" gap={4}>
-              <input
-                type="password"
-                placeholder="Old Password"
-                value={oldPassword}
-                onChange={(e) => setOldPassword(e.target.value)}
-                className="px-4 py-2 border rounded w-full"
-              />
-              <input
-                type="password"
-                placeholder="New Password"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-                className="px-4 py-2 border rounded w-full"
-              />
-              <input
-                type="password"
-                placeholder="Confirm New Password"
-                value={confirmNewPassword}
-                onChange={(e) => setConfirmNewPassword(e.target.value)}
-                className="px-4 py-2 border rounded w-full"
-              />
-              {passwordError && (
-                <TextField value={passwordError} className="text-red-500" />
-              )}
-              {passwordSuccess && (
-                <TextField value={passwordSuccess} className="text-green-500" />
-              )}
-              <Button
-                value="Save"
-                onClick={handleChangePassword}
-                className="px-6 py-3 rounded-lg shadow-md focus:outline-none transition duration-300 border-4 bg-white text-green-500 hover:bg-green-500 hover:text-white"
-              />
-            </Block>
-          )}
-        </Block>
-      )}
+      <Block className="bg-[#2E2E2E] p-6 rounded-lg shadow-md border-4" direction="column" gap={4}>
+        <Button
+          value="Change Password"
+          onClick={() => setIsChangingPassword(!isChangingPassword)}
+          className="px-6 py-3 rounded-lg shadow-md focus:outline-none transition duration-300 border-4 bg-[#1E1E1E] text-white hover:bg-white hover:text-black"
+        />
+        {isChangingPassword && (
+          <Block direction="column" gap={4}>
+            <input
+              type="password"
+              placeholder="Old Password"
+              value={oldPassword}
+              onChange={(e) => setOldPassword(e.target.value)}
+              className="px-4 py-2 border rounded w-full bg-[#121212] text-white"
+            />
+            <input
+              type="password"
+              placeholder="New Password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              className="px-4 py-2 border rounded w-full bg-[#121212] text-white"
+            />
+            <input
+              type="password"
+              placeholder="Confirm New Password"
+              value={confirmNewPassword}
+              onChange={(e) => setConfirmNewPassword(e.target.value)}
+              className="px-4 py-2 border rounded w-full bg-[#121212] text-white"
+            />
+            {passwordError && <TextField value={passwordError} className="text-red-500" />}
+            {passwordSuccess && <TextField value={passwordSuccess} className="text-green-500" />}
+            <Button
+              value="Save"
+              onClick={handleChangePassword}
+              className="px-6 py-3 rounded-lg shadow-md focus:outline-none transition duration-300 border-4 bg-[#1E1E1E] text-green-500 hover:bg-green-500 hover:text-white"
+            />
+          </Block>
+        )}
+      </Block>
     </Block>
   );
 };
--- Client/src/Pages/UserPanel/UserPanel.tsx
+++ Client/src/Pages/UserPanel/UserPanel.tsx
@@ -8,8 +8,6 @@ import BlockTextField from "../../Components/UI/BlockTextField";
 import TextField from "../../Components/UI/TextField";
 import Button from "../../Components/UI/Button";
 
-
-
 const UserPanel = () => {
   const { user } = useAuth();
   const [selectedSubPage, setSelectedSubPage] = useState<"Settings" | "Liked" | "Rated">("Liked");
@@ -17,8 +15,6 @@ const UserPanel = () => {
   const [userImage, setUserImage] = useState("");
   const pages: Array<"Settings" | "Liked" | "Rated"> = ["Settings", "Liked", "Rated"];
 
-
-  // Funkcja do pobrania danych użytkownika
   const fetchUserById = async (id: number) => {
     try {
       const response = await fetch(`${import.meta.env.VITE_API_URL}/user/${id}`, {
@@ -39,16 +35,14 @@ const UserPanel = () => {
     }
   };
 
-  // Uaktualnianie obrazu użytkownika
   useEffect(() => {
     const updateUserImage = async () => {
       if (user?.Id) {
         const fetchedUser = await fetchUserById(user.Id);
         if (fetchedUser?.Picture) {
-          // Zaktualizowanie obrazka z backendu, zakładając, że ścieżka jest względna
           setUserImage(`${import.meta.env.VITE_API_URL}${fetchedUser.Picture}`);
         } else {
-          setIsUserImage(false); // Ustawienie domyślnego obrazu
+          setIsUserImage(false);
         }
       }
     };
@@ -56,7 +50,6 @@ const UserPanel = () => {
     updateUserImage();
   }, [user]);
 
-  // Zapisanie obrazu na serwerze
   const saveImageToDatabase = async (file: File) => {
     if (!user) {
       console.error("User data is not available");
@@ -64,19 +57,19 @@ const UserPanel = () => {
     }
 
     const formData = new FormData();
-    formData.append("file", file); // Dodajemy plik do FormData
+    formData.append("file", file);
 
     try {
       const response = await fetch(`${import.meta.env.VITE_API_URL}/user/${user.Id}/profile-picture`, {
         method: "PATCH",
-        body: formData, // Wysyłamy plik w formacie FormData
+        body: formData,
         credentials: "include",
       });
 
       if (response.ok) {
         alert("Profile picture updated successfully.");
         const updatedUser = await response.json();
-        setUserImage(`${import.meta.env.VITE_API_URL}${updatedUser.Picture}`); // Zaktualizowany URL
+        setUserImage(`${import.meta.env.VITE_API_URL}${updatedUser.Picture}`);
       } else {
         console.error("Failed to update profile picture");
         alert("Failed to update profile picture.");
@@ -87,106 +80,101 @@ const UserPanel = () => {
     }
   };
 
-  // Obsługa zmiany obrazu
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
       const file = event.target.files[0];
       setIsUserImage(false);
-      setUserImage(URL.createObjectURL(file)); // Podgląd obrazu
-      saveImageToDatabase(file); // Zapisujemy obraz na serwerze
+      setUserImage(URL.createObjectURL(file));
+      saveImageToDatabase(file);
     }
   };
 
   return (
-    <Block className="flex-row h-screen bg-white">
-        <Block className="bg-[#121212] py-6 px-6">
-          <LandingNavbar />
-        </Block>
-    
-        <Block direction="column">
-          <Block 
-            className="bg-gray-100 mt-10 flex items-center shadow-md border-4 rounded-lg py-4 w-full sm:w-4/5 lg:w-3/5 mx-auto"
-            direction="row"
-            justify="center"
-          >
-            <Block
-              className="w-48 h-48 mt-16 mb-16 rounded-full overflow-hidden relative border-4 border-black"
-              
-            >
-              {isUserImage ? (
-                <img
-                  src={userImage}
-                  alt="User"
-                  className="w-full h-full object-cover transition duration-300 group-hover:brightness-100 group-hover:opacity-80"
-                />
-              ) : (
-                <Avatar size={180} />
-              )}
-              <label
-                htmlFor="imageUpload"
-                className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 cursor-pointer transition duration-300"
-              >
-                Change Photo
-              </label>
-              <input
-                id="imageUpload"
-                type="file"
-                accept="image/*"
-                className="hidden"
-                onChange={handleImageChange}
-              />
-            </Block>
-            <Block direction="column" className="ml-6 text-center">
-              <BlockTextField
-                value={user?.Name || "Guest"}
-                className="text-2xl font-bold"
-                color="black"
-              />
-              <TextField value="Welcome back!" className="text-gray-600" />
-            </Block>
-          </Block>
-          <Block className="border-black py-4 w-5/6 mx-auto" children={undefined}></Block>
-        </Block>
-    
-        <Block
+    <Block className="flex-row h-screen bg-[#121212]">
+      <Block className="py-6 px-6">
+        <LandingNavbar />
+      </Block>
+
+      <Block direction="column">
+        <Block 
+          className="bg-[#1E1E1E] mt-10 flex items-center shadow-md border-4 rounded-lg py-4 w-full sm:w-4/5 lg:w-3/5 mx-auto"
           direction="row"
           justify="center"
-          align="center"
-          gap="20"
-          className="shadow-lg shadow-[rgba(0,0,0,0.7)] rounded-3xl border-black py-4 w-5/6 mx-auto pt-6 pb-6"
-          style={{
-            backgroundImage: "url('/Black_blinking_stars.gif')",
-            height: "175px",
-          }}
         >
-          {pages.map((page) => (
-            <Button
+          <Block
+            className="w-48 h-48 mt-16 mb-16 rounded-full overflow-hidden relative border-4 border-[#FFFFFF]"
+          >
+            {isUserImage ? (
+              <img
+                src={userImage}
+                alt="User"
+                className="w-full h-full object-cover transition duration-300 group-hover:brightness-100 group-hover:opacity-80"
+              />
+            ) : (
+              <Avatar size={180} />
+            )}
+            <label
+              htmlFor="imageUpload"
+              className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 cursor-pointer transition duration-300"
+            >
+              Change Photo
+            </label>
+            <input
+              id="imageUpload"
+              type="file"
+              accept="image/*"
+              className="hidden"
+              onChange={handleImageChange}
+            />
+          </Block>
+          <Block direction="column" className="ml-6 text-center">
+            <BlockTextField
+              value={user?.Name || "Guest"}
+              className="text-2xl font-bold text-white"
+            />
+            <TextField value="Welcome back!" className="text-gray-400" />
+          </Block>
+        </Block>
+        <Block className="border-[#1F1F1F] py-4 w-5/6 mx-auto" children={undefined}></Block>
+      </Block>
+
+      <Block
+        direction="row"
+        justify="center"
+        align="center"
+        gap="6"
+        className="shadow-lg shadow-[rgba(255,255,255,0.3)] rounded-3xl border-[#1F1F1F] py-3 w-5/6 mx-auto pt-6 pb-6 h-[175px]" 
+        style={{
+          backgroundImage: "url('/Black_blinking_stars.gif')",
+        }}
+      >
+
+        {pages.map((page) => (
+          <Button
             key={page}
             onClick={() => setSelectedSubPage(page)}
             className={`px-6 py-3 rounded-lg shadow-md focus:outline-none transition-all duration-300 border-4 ${
               selectedSubPage === page
-                ? "bg-black text-white"
-                : "bg-white text-black hover:bg-black hover:text-white"
+                ? "bg-white text-black"
+                : "bg-[#1F1F1F] text-white hover:bg-white hover:text-black"
             }`}
             value={page}
             type="button"
             id={`button-${page}`}
-            disabled={false} // lub dynamicznie ustawione
+            disabled={false}
           />
-          
-          ))}
-        </Block>
-    
-        <Block align="center" className="mt-6">
-          <Block className="w-3/5">
-            {selectedSubPage === "Settings" && <UserSettings />}
-            {selectedSubPage === "Liked" && <div>Liked Content</div>}
-            {selectedSubPage === "Rated" && <div>Rated Content</div>}
-          </Block>
+        ))}
+      </Block>
+
+      <Block align="center" className="mt-6 bg-[#121212]">
+        <Block className="w-3/5">
+          {selectedSubPage === "Settings" && <UserSettings />}
+          {selectedSubPage === "Liked" && <div className="text-white">Liked Content</div>}
+          {selectedSubPage === "Rated" && <div className="text-white">Rated Content</div>}
         </Block>
       </Block>
-    );
-  
+    </Block>
+  );
 };
 
 export default UserPanel;
