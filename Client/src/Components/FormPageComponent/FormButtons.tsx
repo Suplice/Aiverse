b369e7176d6ae96cc250e093d3d158909b7f6507
Mateusz Duma <mateuszsuplice@gmail.com>
2025-02-08 15:50:21 +0100

    feat: implement navigation on cancel button and enhance form submission handling in MainForm and FormButtons components

--- Client/src/Components/FormPageComponent/FormButtons.tsx
+++ Client/src/Components/FormPageComponent/FormButtons.tsx
@@ -1,27 +1,29 @@
-const FormButtons = () => {
-    const handleCancel = () => {
-      console.log("Formularz anulowany");
-    };
-  
-    return (
-        <div className="form-buttons flex justify-end gap-4 mt-4">
+import { useNavigate } from "react-router";
 
-    <button
-      type="button"
-      className="bg-gray-500 text-white py-2 px-4 rounded-lg hover:bg-gray-700 duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-gray-800"
-      onClick={handleCancel}
-    >
-      Cancel
-    </button>
-    <button
-      type="submit"
-      className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700 duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-gray-800"
-    >
-      Submit
-    </button>
-    
-  </div>
-    );
+const FormButtons = () => {
+  const navigate = useNavigate();
+
+  const handleCancel = () => {
+    navigate(-1);
   };
-  
-  export default FormButtons;
+
+  return (
+    <div className="form-buttons flex justify-end gap-4 mt-4">
+      <button
+        type="button"
+        className="bg-gray-500 text-white py-2 px-4 rounded-lg hover:bg-gray-700 duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-gray-800"
+        onClick={handleCancel}
+      >
+        Cancel
+      </button>
+      <button
+        type="submit"
+        className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700 duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-gray-800"
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default FormButtons;
