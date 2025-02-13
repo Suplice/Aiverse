19a474997cf8476dbd5cb0a9d5d749f950b83cd6
Mateusz Duma <mateuszsuplice@gmail.com>
2025-02-13 15:01:22 +0100

    feat: refactor ServiceGallery component to use ImageGallery and update gallery image types

--- Client/src/Components/AIViewPageComponents/AIViewPageMain.tsx
+++ Client/src/Components/AIViewPageComponents/AIViewPageMain.tsx
@@ -4,13 +4,13 @@ import ServiceDetail from "./ServiceDetail";
 import CommentSection from "../CommentSection/CommentSection";
 import { useEffect, useState } from "react";
 import FullDescriptionComponent from "./FullDescriptionComponent";
-import ServiceGallery from "./ServiceGallery";
+import ServiceGallery, { GalleryImage } from "./ServiceGallery";
 import PageNavigation from "./PageNavigation";
 
 const AIViewPageMain = () => {
   const { id } = useParams<{ id: string }>();
   const { services } = useAiService();
-  const [galleryImages, setGalleryImages] = useState<string[]>([]);
+  const [galleryImages, setGalleryImages] = useState<GalleryImage[]>([]);
   const [, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -35,8 +35,13 @@ const AIViewPageMain = () => {
           throw new Error(data.message || "Failed to fetch gallery images");
         }
 
-        console.log("Zdjęcia:" + data);
-        setGalleryImages(data.data);
+        console.log("Zdjęcia:" + data.data);
+        setGalleryImages(
+          data.data.map((image: string) => ({
+            original: import.meta.env.VITE_API_URL + image,
+            thumbnail: import.meta.env.VITE_API_URL + image,
+          }))
+        );
       } catch {
         console.log("error");
       } finally {
@@ -60,7 +65,7 @@ const AIViewPageMain = () => {
   }
 
   return (
-    <div className="bg-[#121212] p-6 max-w-[1500px] text-white flex flex-col w-full self-center gap-8">
+    <div className="bg-[#121212] p-6 max-w-[1500px] text-white flex flex-col w-full self-center gap-12">
       <ServiceDetail service={service} />
       <PageNavigation />
       <div id="full-description">
